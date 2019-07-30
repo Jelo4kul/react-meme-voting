5f1b7d868a294086b7de553a0aedd99417ce7400
jelo <jelo4kul@gmail.com>
2019-07-30 12:30:00 +0100

    Updates

---
+++ precache-manifest.81cf0740838fc7e152ef05d10ed2eaec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4738ce7bb599aa9b97a061e3cc88406",
+    "url": "/react-meme-voting/index.html"
+  },
+  {
+    "revision": "802251b668dfff6734e5",
+    "url": "/react-meme-voting/static/css/main.1ad2bbd1.chunk.css"
+  },
+  {
+    "revision": "8f865c8fb3df119a2a1a",
+    "url": "/react-meme-voting/static/js/2.9163d81f.chunk.js"
+  },
+  {
+    "revision": "802251b668dfff6734e5",
+    "url": "/react-meme-voting/static/js/main.a4e941be.chunk.js"
+  },
+  {
+    "revision": "021289a9257739109f45",
+    "url": "/react-meme-voting/static/js/runtime~main.0b8891af.js"
+  }
+]);
